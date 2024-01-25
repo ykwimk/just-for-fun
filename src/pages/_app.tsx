387b913c027def568bd66155877d453590b3492c
ykwimk <ykwimk@gmail.com>
2024-01-25 15:26:26 +0900

    chore: Radix UI 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import '@radix-ui/themes/styles.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
