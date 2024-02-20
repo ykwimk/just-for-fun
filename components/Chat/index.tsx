69a07be5601579618c8f697957da448bb25011bc
ykwimk <ykwimk@gmail.com>
2024-02-20 12:28:40 +0900

    fix: config 수정 및 컴포넌트 생성

--- components/Chat/index.tsx
+++ components/Chat/index.tsx
@@ -1,12 +1,11 @@
-import { Input } from '@/components/ui/input';
+import ChatView from './ChatView';
+import ChatInput from './ChatInput';
 
 export default function Chat() {
   return (
     <div className="p-10">
-      <div className="mb-8">채팅창</div>
-      <div>
-        <Input />
-      </div>
+      <ChatView />
+      <ChatInput />
     </div>
   );
 }
