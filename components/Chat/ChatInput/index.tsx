8c9a4b6cb062194e7168d6a9bf09752802f77bbb
ykwimk <ykwimk@gmail.com>
2024-02-20 12:38:15 +0900

    feat: UI Button 추가

--- components/Chat/ChatInput/index.tsx
+++ components/Chat/ChatInput/index.tsx
@@ -1,5 +1,11 @@
+import { Button } from '@/app/ui/button';
 import { Input } from '@/components/ui/input';
 
 export default function ChatInput() {
-  return <Input type="text" />;
+  return (
+    <div className="flex w-full min-w-sm items-center space-x-2">
+      <Input type="text" />
+      <Button type="submit">Subscribe</Button>
+    </div>
+  );
 }
